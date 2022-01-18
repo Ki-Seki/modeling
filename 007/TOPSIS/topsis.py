c0a4c90b42f689b025db60bcb9619ea993a0b8a0
Ki-Seki <Ki_Seki@outlook.com>
2022-01-18 16:58:50 +0800

    完成阅读 topsis.py

--- 007/TOPSIS/topsis.py
+++ 007/TOPSIS/topsis.py
@@ -1,3 +1,8 @@
+"""
+TOPSIS法(优劣解距离法)介绍及 python3 实现 - Suranyi的文章 - 知乎
+https://zhuanlan.zhihu.com/p/37738503
+"""
+
 import pandas as pd
 import numpy as np
 
@@ -58,23 +63,34 @@ def dataDirection_3(datas, x_min, x_max, x_minimum, x_maximum):
 
     return list(map(normalization, datas))
 
-################# TODO #######################
 
 def entropyWeight(data):
-	data = np.array(data)
-	# 归一化
-	P = data / data.sum(axis=0)
+    """
+    熵权法确定权重
 
-	# 计算熵值
-	E = np.nansum(-P * np.log(P) / np.log(len(data)), axis=0)
+    :param data: 行为评价对象，列为一个个的指标的 DataFrame
+    """
+    data = np.array(data)
+    # 归一化
+    P = data / data.sum(axis=0)
 
-	# 计算权系数
-	return (1 - E) / (1 - E).sum()
+    # 计算熵值
+    E = np.nansum(-P * np.log(P) / np.log(len(data)), axis=0)
+
+    # 计算权系数
+    return (1 - E) / (1 - E).sum()
 
 
 def topsis(data, weight=None):
+    """
+    TOPSIS 评价
+
+    :param data: 数据
+    :param weight: 权重，若不给，则用熵权法自动确定
+    """
+
     # 归一化
-    data = data / np.sqrt((data ** 2).sum())
+    data = data / ((data ** 2).sum())**0.5
 
     # 最优最劣方案
     Z = pd.DataFrame([data.min(), data.max()], index=['负理想解', '正理想解'])
@@ -94,16 +110,21 @@ def topsis(data, weight=None):
     return Result, Z, weight
 
 
-def main():
-    data = pd.DataFrame(
-        {'人均专著': [0.1, 0.2, 0.4, 0.9, 1.2], 
-        '生师比': [5, 6, 7, 10, 2], 
-        '科研经费': [5000, 6000, 7000, 10000, 400], 
-        '逾期毕业率': [4.7, 5.6, 6.7, 2.3, 1.8]}, 
-        index=['院校' + i for i in list('ABCDE')])
+if __name__ == '__main__':
+    """
+    问题描述
+    给定 5 所院校的 4 个指标，评价这 5 个院校的好坏
+    """
 
-    data['生师比'] = dataDirection_3(data['生师比'], 5, 6, 2, 12)   # 师生比数据为区间型指标
-    data['逾期毕业率'] = 1 / data['逾期毕业率']   # 逾期毕业率为极小型指标
+    data = {'人均专著': [0.1, 0.2, 0.4, 0.9, 1.2],
+            '生师比': [5, 6, 7, 10, 2],
+            '科研经费': [5000, 6000, 7000, 10000, 400],
+            '逾期毕业率': [4.7, 5.6, 6.7, 2.3, 1.8]}
+    data = pd.DataFrame(data, index=['院校' + i for i in list('ABCDE')])
 
+    data['生师比'] = dataDirection_3(data['生师比'], 5, 6, 2, 12)  # 师生比数据为区间型指标
+    data['逾期毕业率'] = dataDirection_1(data['逾期毕业率'], 0.01)  # 逾期毕业率为极小型指标
 
-    out = topsis(data, weight=[0.2, 0.3, 0.4, 0.1])    # 设置权系数
+    weight = [0.2, 0.3, 0.4, 0.1]  # 人为划分的权重
+    out = topsis(data, weight=None)  # 若为 None，使用熵权法确定权重
+    print(out[0])
