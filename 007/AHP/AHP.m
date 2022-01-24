3eacd391a4d831723ef2b2c8599d6d337dfaaf44
Ki-Seki <Ki_Seki@outlook.com>
2022-01-24 12:54:41 +0800

    update AHP

--- 007/AHP/AHP.m
+++ 007/AHP/AHP.m
@@ -1,35 +1,24 @@
-function [Q]=AHP(B)
-% 参数：B为正互反阵或成对比较阵；输出：是否通过一致性检验；返回值：Q为权向量
-[n,~]=size(B);
-%判别矩阵具有完全一致性
-for i=1:n
-    for j=1:n
-        if B(i,j)*B(j,i)~=1   
-        fprintf('i=%d,j=%d,B(i,j)=%d,B(j,i)=%d\n',i,j,B(i,j),B(j,i))  
-        end  
-    end
-end
-%求特征值特征向量,找到最大特征值对应的特征向量
-[V,D]=eig(B);
-tz=max(D);
-tzz=max(tz);
-c1=find(D(1,:)==max(tz));
-tzx=V(:,c1);%特征向量
-%权
-quan=zeros(n,1);
-for i=1:n
-quan(i,1)=tzx(i,1)/sum(tzx);
-end
-Q=quan;
-%一致性检验
-CI=(tzz-n)/(n-1);
-RI=[0,0,0.58,0.9,1.12,1.24,1.32,1.41,1.45,1.49,1.52,1.54,1.56,1.58,1.59];
-%判断是否通过一致性检验
-CR=CI/RI(1,n);
-if CR>=0.1
-   fprintf('没有通过一致性检验\n');
-else
-  fprintf('通过一致性检验\n');
-end
- 
+% 根据正互反阵得到权重列向量
+function [weight, flag, CR] = AHP(matrix)
+    % matrix 正互反阵
+    % weight 权重，列向量
+    % flag 是否通过一致性检验，通过为 true
+    % CR 层次分析法中的一致性比率
+
+    n = size(matrix, 1);
+    
+    %% 求最大特征值及其对应的特征向量
+    [vec, val] = eig(matrix);
+    [max_eigval, index] = max(diag(val));  % 最大特征值及其下标
+    max_eigvec = vec(:, index);  % 最大特征值对应的特征向量
+    
+    %% 求权重
+    
+    weight = max_eigvec / sum(max_eigvec);
+    
+    %% 一致性检验
+    CI = (max_eigval - n) / (n - 1);
+    RI = [0,0,0.58,0.9,1.12,1.24,1.32,1.41,1.45,1.49,1.52,1.54,1.56,1.58,1.59];
+    CR = CI / RI(n);
+    flag = (CR <= 0.1);
 end
